c1196bee0d100a13c451ac97146d1bbacef25715
Erick Almeida <erickprofissional@gmail.com>
2020-04-02 18:20:12 -0300

    Classes criadas

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,29 @@
+/*
+ * Fazer um programa para ler o caminho de um arquivo .csv 
+ *contendo os dados de itens vendidos. Cada item possui um 
+ *nome, preço unitário e quantidade, separados por vírgula. 
+ *Você deve gerar um novo arquivo chamado "summary.csv", localizado 
+ *em uma subpasta chamada "out" a partir da pasta original do 
+ *arquivo de origem, contendo apenas o nome e o valor total para 
+ *aquele item (preço unitário multiplicado pela quantidade),
+ */
+
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa 
+{
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+	}
+}
