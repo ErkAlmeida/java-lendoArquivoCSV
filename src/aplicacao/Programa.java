394bfacd559359e724af96da0587e70903ffeb65
Erick Almeida <erickprofissional@gmail.com>
2020-04-02 21:33:15 -0300

    Leitura do arquivo efetuada

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,20 +10,53 @@
 
 package aplicacao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entidades.Itens;
+
 public class Programa 
 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
 		Locale.setDefault(Locale.US);
 		
 		Scanner sc = new Scanner(System.in);
 		
+		String lerArquivo = "C:\\WorkSpace\\eclipse\\LendoArquivoCSV\\arquivos\\in.csv";
+		
+		List<Itens> listItens = new ArrayList<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(lerArquivo)))
+		{
+			String line = br.readLine();
+			while(line != null)
+			{
+				String item[] = line.split(",");
+				
+				String nome = item[0];
+				double preco = Double.parseDouble(item[1]);
+				int quantidade = Integer.parseInt(item[2]);
+				
+				listItens.add(new Itens(nome, preco, quantidade));
+				
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch(IOException erro)
+		{
+			System.out.println("Erro: "+ erro.getMessage());
+		}
 		
 		
-		
-		sc.close();
+						
+		//sc.close();
 	}
 }
